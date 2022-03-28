d15cb64a9a4bd4f934d0eba6089d26ed831a30f6
David Xu <62825020+dxaviud@users.noreply.github.com>
2022-03-28 01:02:24 -0400

    fix bug (var name conflict)

--- src/constants.php
+++ src/constants.php
@@ -3,10 +3,10 @@ if (isset($_SERVER['RDS_HOSTNAME'])) {
     $dbhost = $_SERVER['RDS_HOSTNAME'];
     $dbport = $_SERVER['RDS_PORT'];
     $dbname = $_SERVER['RDS_DB_NAME'];
-    $username = $_SERVER['RDS_USERNAME'];
-    $password = $_SERVER['RDS_PASSWORD'];
+    $dbusername = $_SERVER['RDS_USERNAME'];
+    $dbpassword = $_SERVER['RDS_PASSWORD'];
 
-    define("CONNECTION_STRING", "host=$dbhost port=$dbport dbname=$dbname user=$username password=$password");
+    define("CONNECTION_STRING", "host=$dbhost port=$dbport dbname=$dbname user=$dbusername password=$dbpassword");
 } else {
     define("CONNECTION_STRING", "host=localhost port=5432 dbname=php-todolist user=postgres password=postgres");
 }
